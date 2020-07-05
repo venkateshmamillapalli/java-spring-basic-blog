badb43f415ac30554fb13eedc01d139914ac29d0
venkateshmamillapalli <venkat.141367@gmail.com>
2020-07-05 17:47:39 +0530

    module 2

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,30 @@
 package com.pluralsight.blog;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
+
 @Controller
 public class BlogController {
+	
+	private PostRepository postRepository;
+	
+	public BlogController(PostRepository postRepository) {
+		this.postRepository = postRepository;
+	}
 
 	@RequestMapping("/")
 	public String listPosts(ModelMap map) {
+		List<Post> posts = postRepository.getAllPosts();
 		
 		map.put("title","Blog Post 1");
+		map.put("posts",posts);
+		
 		return "home";
 	}
 }
